5615e0833dc7f555c7cf4b76773032bcd90c7537
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-04-29 12:38:01 -0300

    add contentful

---
+++ src/contentful/index.ts
@@ -0,0 +1,6 @@
+import * as contentfulClient from "contentful";
+
+export default contentfulClient.createClient({
+  space: "nj59inj60l6s",
+  accessToken: "bjKVz8OG7q1w2eCetcsfuhsxMQM2szrP9QKBZg62I2Y",
+});
