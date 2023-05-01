73845066cea518695af5cbb3038b8c79c46892d5
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-05-01 20:24:10 -0300

    add contentful types generation

--- src/contentful/index.ts
+++ src/contentful/index.ts
@@ -1,6 +1,38 @@
-import * as contentfulClient from "contentful";
+import { EntrySkeletonType, createClient } from "contentful";
+import { config } from "dotenv";
+import { CONTENT_TYPE } from "./generated/contentful";
 
-export default contentfulClient.createClient({
-  space: "nj59inj60l6s",
-  accessToken: "bjKVz8OG7q1w2eCetcsfuhsxMQM2szrP9QKBZg62I2Y",
-});
+/*
+ * We tell TypeScript that those environment variables are always defined.
+ * If you want to learn more about this madness, read:
+ * https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
+ */
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      CONTENTFUL_SPACE_ID: string;
+      CONTENTFUL_DELIVERY_API_ACCESS_TOKEN: string;
+    }
+  }
+}
+
+config();
+
+export default class ContentService {
+  static get instance() {
+    return new ContentService();
+  }
+
+  client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID ?? "",
+    accessToken: process.env.CONTENTFUL_DELIVERY_API_ACCESS_TOKEN ?? "",
+  });
+
+  async getEntriesByType(type: CONTENT_TYPE) {
+    return (
+      await this.client.getEntries({
+        content_type: type,
+      })
+    ).items;
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,42 @@
-import contentfulClient from "@/contentful";
-import { AssetLink, ChainModifiers, EntryCollection } from "contentful";
+import ContentService from "@/contentful";
+import { ITheyTrustUsCompaniesFields } from "@/contentful/generated/contentful";
+import Image from "next/image";
 
 interface HomeProps {
-  theyTrustUsCompanies: any;
+  theyTrustUsCompanies: ITheyTrustUsCompaniesFields[];
 }
 
 export async function getStaticProps() {
-  // This will run when the page is statically generated in Vercel.
-  console.log("static props");
-
-  // TODO: learn how to query for the fields I want. I only want the they trust us ones.
-  const theyTrustUsCompanies = await contentfulClient.getEntries();
-  console.log(theyTrustUsCompanies);
-
-  const props: HomeProps = {
-    theyTrustUsCompanies,
-  };
+  const theyTrustUsCompanies = (
+    await ContentService.instance.getEntriesByType("theyTrustUsCompanies")
+  ).map((entry) => entry.fields);
   return {
-    props,
+    props: {
+      theyTrustUsCompanies,
+    },
   };
 }
 
-export default function Home(props: HomeProps) {
-  console.log(props.theyTrustUsCompanies.items);
+export default function Home({ theyTrustUsCompanies }: HomeProps) {
+  console.log();
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
     >
-      {props.theyTrustUsCompanies.items.map((item: any, index: number) => {
-        return (
-          <div key={index}>
-            <div>{JSON.stringify(item.fields.icon?.fields.file?.url)}</div>
-            <div>{JSON.stringify(item.fields.name)}</div>
-          </div>
-        );
-      })}
+      <div className="flex">
+        {theyTrustUsCompanies.map((item, index: number) => {
+          return (
+            <div key={index}>
+              <Image
+                src={item.icon?.fields.file?.url as string}
+                alt={item.icon?.fields.description as string}
+                width="200"
+                height="85"
+              />
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
